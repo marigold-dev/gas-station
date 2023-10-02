ac14958ed13fa2a95df232ad0dc8e4c20f1f3325
Arthur Guillon <arthur@marigold.dev>
2023-10-02 09:57:53 +0200

    Find the fees in the resulting transaction and update credits accordingly for all contracts

--- src/main.py
+++ src/main.py
@@ -46,6 +46,24 @@ app.add_middleware(
 )
 
 
+def find_fees(global_tx, payer_key):
+    """Find the baker and storage fees in the operation result
+    returns the operation indice as well, as the length of the fees
+    of one operation in the bulk may vary"""
+    op_result = global_tx["contents"]
+    print(op_result)
+    fees = [
+        (z, x["destination"])
+          for x in op_result
+          for y in (x["metadata"].get("operation_result", {}).get("balance_updates", {}),
+                    x["metadata"].get("balance_updates", {}))
+          for z in y
+          if z.get("contract", "") == payer_key
+    ]
+    print(fees)
+    return fees
+
+
 class TezosManager:
     def __init__(self, ptz):
         self.ops_queue = OrderedDict()
@@ -63,6 +81,21 @@ class TezosManager:
             # TODO wait the right amount of time
             await asyncio.sleep(self.block_time)
         return self.results[sender]
+    async def update_fees(self, posted_tx):
+        nb_try = 0
+        while nb_try < 4:
+            try:
+                op_result = ptz.shell.blocks[-10:].find_operation(posted_tx.hash())
+                break
+            except Exception as _e:
+                nb_try += 1
+                await asyncio.sleep(self.block_time)
+        else:
+            raise Exception(f"Couldn't find operation {posted_tx.hash()}")
+        fees = find_fees(op_result, ptz.key.public_key_hash())
+        # TODO group requests
+        for (fee, contract) in fees:
+            db.update_credit(contract, int(fee["change"]))
 
     async def main_loop(self):
         while True:
