b14a566a1d101cc12ff31d3e3ca2be5ca1190cd5
Quentin Burg <quentin@marigold.dev>
2023-10-19 17:51:20 +0200

    :wrench: allow all origins

--- src/main.py
+++ src/main.py
@@ -11,14 +11,15 @@ app = FastAPI()
 
 app.include_router(routes.router)
 
-origins = [
-    "http://localhost:5173",
-    "http://localhost:3000"
-]
+# TODO:https://github.com/marigold-dev/gas-station/issues/3
+# origins = [
+#     "http://localhost:5173",
+#     "http://localhost:3000"
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
