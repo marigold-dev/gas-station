204b02f8ca8a5b3b58634a6d363b18655122fdb0
Arthur Guillon <arthur@marigold.dev>
2024-03-15 11:56:36 +0100

    fix: time not always set correctly when creating conditions and operations

--- src/models.py
+++ src/models.py
@@ -9,6 +9,7 @@ from sqlalchemy import (
     String,
 )
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
@@ -144,7 +145,7 @@ class Operation(Base):
     entrypoint_id = Column(UUID(as_uuid=True), ForeignKey("entrypoints.id"))
     hash = Column(String)
     status = Column(String)  # TODO Enum
-    created_at = Column(DateTime(timezone=True), default=datetime.datetime.utcnow())
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     contract = relationship("Contract", back_populates="operations")
     entrypoint = relationship("Entrypoint", back_populates="operations")
@@ -182,7 +183,7 @@ class Condition(Base):
     max = Column(Integer, nullable=False)
     current = Column(Integer, nullable=False)
     created_at = Column(
-        DateTime(timezone=True), default=datetime.datetime.utcnow(), nullable=False
+        DateTime(timezone=True), server_default=func.now(), nullable=False
     )
     is_active = Column(Boolean, nullable=False)
     contract = relationship("Contract", back_populates="conditions")
